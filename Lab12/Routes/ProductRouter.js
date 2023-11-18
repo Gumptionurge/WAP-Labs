cb07a2bc19e5ec1a7bae10954f889604a494c4a2
Gumptionurge <113231578+Gumptionurge@users.noreply.github.com>
2023-11-17 21:03:32 -0600

    Update ProductRouter.js

--- Lab12/Routes/ProductRouter.js
+++ Lab12/Routes/ProductRouter.js
@@ -12,28 +12,3 @@ res.send(`${req.body.productname}, ${req.body.quantity},${req.body.price}`);
 });
 module.exports=router;
 
-/*
-const express = require('express');
-const path = require('path');
-
-const router = express.Router();
-
-router.get('/add', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
-});
-
-router.post('/save', (req, res) => {
-    res.send(`${req.body.title}, ${req.body.price}`);
-});
-
-router.delete('/delete', (req, res) => {
-    res.send(`${req.body.title}, ${req.body.price}`);
-});
-
-
-router.put('/edit', (req, res) => {
-    res.send(`${req.body.title}, ${req.body.price}`);
-});
-
-module.exports = router;
-*/
